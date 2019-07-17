2f3a5d2f4391be01925292e7f30522c17eb39baf
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 01:10:46 +0600

    ALter Primary Key

---
+++ alterprimarykey.js
@@ -0,0 +1,13 @@
+var mysql      = require('mysql');
+var sql = "ALTER TABLE Customers ADD COLUMN ID INT AUTO_INCREMENT PRIMARY KEY";
+var connection = mysql.createConnection({
+	database : 'NodeDB',
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Primary Key!");
+});
