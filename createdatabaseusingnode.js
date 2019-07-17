5c30627cca197c180fb77479d37a1ba8f343030c
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 01:04:04 +0600

    Create & Drop DB, Create Table

---
+++ createdatabaseusingnode.js
@@ -0,0 +1,11 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query("CREATE DATABASE NodeDB;", function (err, result) {
+    if (err) throw err;
+    console.log("Database Created!");
+});
