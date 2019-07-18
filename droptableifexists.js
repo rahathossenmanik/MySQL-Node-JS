d92092f343befaa26eec97c6cdc522ec7b3d269f
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 13:45:42 +0600

    Drop Table

---
+++ droptableifexists.js
@@ -0,0 +1,13 @@
+var mysql      = require('mysql');
+var sql = "DROP TABLE IF EXISTS Demo";
+var connection = mysql.createConnection({
+	database : 'NodeDB',
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table Deleted!");
+});
