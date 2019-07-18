b8b094180d441f3cce5fdd7578abb43ddadd1adc
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 14:15:00 +0600

    Joins

---
+++ createsecondtable.js
@@ -0,0 +1,13 @@
+var mysql      = require('mysql');
+var sql = "CREATE TABLE Salesman (name VARCHAR(255), Address VARCHAR(255))";
+var connection = mysql.createConnection({
+	database : 'NodeDB',
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table Created!");
+});
---
+++ sqlrightjoin.js
@@ -0,0 +1,13 @@
+var mysql      = require('mysql');
+var sql = "SELECT Customers.Name AS Name, Salesman.Address AS Address FROM Customers RIGHT JOIN Salesman ON Customers.Name = Salesman.Name";
+var connection = mysql.createConnection({
+	database : 'NodeDB',
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
