a0acabbe07cd639a609b910ef74b65338ce05b51
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 13:10:12 +0600

    Order By

---
+++ orderby.js
@@ -0,0 +1,13 @@
+var mysql      = require('mysql');
+var sql = "SELECT Name, Address FROM Customers ORDER BY Name";
+var connection = mysql.createConnection({
+	database : 'NodeDB',
+    host     : 'localhost',
+	port	 : '3306',
+    user     : 'root',
+    password : ''
+});
+connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
